f6d28ba54a650dd4007dedc33b969ccb82da08fb
naoufalHdr1 <naoufalhadra96@gmail.com>
2024-11-06 21:09:51 +0100

    Task 3: fix check 4 error

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -70,39 +70,23 @@ def get_logger() -> logging.Logger:
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    """Creates a connector to a database.
     """
+    Connect to the MySQL database using credentials from environment
+    variables and return the connection object.
+    """
+    # Fetching environment variables, using defaults if not set
     db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
     db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
     db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
 
-    connection = mysql.connector.connect(
-        host=db_host,
-        port=3306,
-        user=db_user,
-        password=db_pwd,
-        database=db_name,
-    )
-    return connection
-
-"""
-def get_db():
-    Connect to the MySQL database using credentials from environment
-    variables and return the connection object.
-    # Fetching environment variables, using defaults if not set
-    db_username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
-    db_password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
-    db_host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
-    db_name = os.getenv('PERSONAL_DATA_DB_NAME', '')
-
     # Establishing a connection to the database
     connection = mysql.connector.connect(
-            user=db_username,
-            password=db_password,
+            port=3306,
+            user=db_user,
+            password=db_pwd,
             host=db_host,
             database=db_name
     )
 
     return connection
-"""
