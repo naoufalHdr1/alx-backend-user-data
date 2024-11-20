cec74d1e358b949a1ff4f196344e3ad392ec0391
naoufalHdr1 <naoufalhadra96@gmail.com>
2024-11-20 14:23:49 +0100

    Task 19: fix a NameError

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -102,7 +102,7 @@ def update_password():
     except ValueError:
         abort(403)
 
-    return jsonfiy({"email": email, "message": "Password updated"}), 200
+    return jsonify({"email": email, "message": "Password updated"}), 200
 
 
 if __name__ == "__main__":
