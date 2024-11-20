9221a7817708525a11a5d857bddcccddde725d8d
naoufalHdr1 <naoufalhadra96@gmail.com>
2024-11-20 03:23:44 +0100

    Task 16: handle exception errors

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -86,10 +86,14 @@ class Auth:
             return None
         self._db.update_user(user_id, session_id=None)
 
-    def get_reset_password_token(email: str) -> str:
+    def get_reset_password_token(self, email: str) -> str:
         """ Generate and return a reset password token for the given email.
         """
-        user = self._db.find_user_by(email=email)
+        try:
+            user = self._db.find_user_by(email=email)
+        except Exception:
+            user = None
+
         if not user:
             raise ValueError()
 
